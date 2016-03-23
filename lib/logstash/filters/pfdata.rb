c88b8730637f7b0fabf9b7a69650fcc0ff46e655
huangchaosuper <huangchaosuper@gmail.com>
2016-03-23 12:54:28 +0800

    add

--- lib/logstash/filters/pfdata.rb
+++ lib/logstash/filters/pfdata.rb
@@ -4,21 +4,7 @@ require "logstash/namespace"
 require "logstash/json"
 require "logstash/timestamp"
 
-# This example filter will replace the contents of the default 
-# message field with whatever you specify in the configuration.
-#
-# It is only intended to be used as an example.
 class LogStash::Filters::Pfdata < LogStash::Filters::Base
-
-  # Setting the config_name here is required. This is how you
-  # configure this filter from your Logstash config.
-  #
-  # filter {
-  #   example {
-  #     message => "My message..."
-  #   }
-  # }
-  #
   config_name "pfdata"
   
   # The string to split on. This is usually a line terminator, but can be any
@@ -36,40 +22,30 @@ class LogStash::Filters::Pfdata < LogStash::Filters::Base
 
   public
   def register
-    # Add instance variables 
+    # null
   end # def register
 
   public
   def filter(event)
-    
     original_value = event[@field]
     return unless original_value
-
+    parsed = "[]";
     begin
-      parsed = "{}";
-      parsed = Base64.decode64(parsed) if parsed != '-'
+      parsed = Base64.decode64(original_value) if original_value != '-'
       json_value = LogStash::Json.load(parsed)
     rescue => e
       @tag_on_failure.each{|tag| event.tag(tag)}
-      @logger.warn("Error parsing json", :field => @field, :raw => field, :exception => e)
-      return
+      json_value = LogStash::Json.load("[]")
     end
-
-
 
     if json_value.is_a?(Array)
       splits = json_value
     elsif json_value.is_a?(String)
-      # Using -1 for 'limit' on String#split makes ruby not drop trailing empty
-      # splits.
       splits = json_value.split(@terminator, -1)
+      return if splits.length == 1
     else
-      raise LogStash::ConfigurationError, "Only String and Array types are splittable. field:#{@field} is of type = #{original_value.class}"
+      splits = Array.new(1,"NA")
     end
-
-    # Skip filtering if splitting this event resulted in only one thing found.
-    return if splits.length == 1 && json_value.is_a?(String)
-    #or splits[1].empty?
 
     splits.each do |value|
       next if value.empty?
